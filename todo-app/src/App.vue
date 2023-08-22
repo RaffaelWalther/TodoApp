dc80a6239781edd3760111f358569bfc1c067fa4
rwalther <raffael.walther@bluewin.ch>
2023-08-22 23:26:57 +0200

    Basic functionality of todo app

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,6 +1,40 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+
+  interface TodoItem {
+    name : string
+    checked : boolean
+  }
+
+  const todoName = ref("")
+  const todos = ref<TodoItem[]>([]);
+
+  function OnEnter() {
+    todos.value.push({ name : todoName.value, checked : false });
+    todoName.value = "";
+  }
+
+  function OnDelete(index : number){
+    todos.value.splice(index, 1);
+  }
+
 </script>
 
 <template>
-  Hello
+  <h1>todos</h1>
+  <input v-model="todoName" @keyup.enter="OnEnter" />
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.name">
+      <input type="checkbox" v-model="todo.checked" />
+      <span :class="{checked : todo.checked}" >{{ todo.name }}</span>
+      <button @click.stop.prevent="OnDelete(index)">x</button>
+    </li>
+  </ul>
+
 </template>
+
+<style scoped>
+  .checked{
+    text-decoration: line-through;
+  }
+</style>
